270a4806a5d155e94676a813097405984ba46fbe
olu-git <olumideykehinde@gmail.com>
2026-08-01 21:07:33 +1000

    Mark Send-Off booths sold out

--- js/booths.js
+++ js/booths.js
@@ -131,15 +131,15 @@ const CONFIG = {
     'The Send-Off (17 Aug)': [
       { name: 'VVIP Lounges', groups: [
         { name: 'XL', capacity: '12 people', booths: [
-          { code: 'VVIP XL', label: 'XL', price: '$854' }
+          { code: 'VVIP XL', label: 'XL', price: '$854', sold: true }
         ]},
         { name: 'Large (L)', capacity: '10 people', booths: [
-          { code: 'VVIP L1', label: 'L1', price: '$854' },
+          { code: 'VVIP L1', label: 'L1', price: '$854', sold: true },
           { code: 'VVIP L2', label: 'L2', price: '$854' }
         ]},
         { name: 'Medium (M)', capacity: '8 people', booths: [
           { code: 'VVIP M1', label: 'M1', price: '$610' },
-          { code: 'VVIP M2', label: 'M2', price: '$610' },
+          { code: 'VVIP M2', label: 'M2', price: '$610', sold: true },
           { code: 'VVIP M3', label: 'M3', price: '$854', sold: true },
           { code: 'VVIP M4', label: 'M4', price: '$854' },
           { code: 'VVIP M5', label: 'M5', price: '$610' }
@@ -157,7 +157,7 @@ const CONFIG = {
         { code: 'VIP 1', label: '1', price: '$488' },
         { code: 'VIP 2', label: '2', price: '$488' },
         { code: 'VIP 3', label: '3', price: '$488' },
-        { code: 'VIP 4', label: '4', price: '$488' },
+        { code: 'VIP 4', label: '4', price: '$488', sold: true },
         { code: 'VIP 5', label: '5', price: '$488' },
         { code: 'VIP 6', label: '6', price: '$488' }
       ]},
