8eb9616387293e29f18bfe5edf42a3d06d494359
olu-git <olumideykehinde@gmail.com>
2026-08-17 12:58:59 +0800

    Update Send-Off booth availability

--- js/booths.js
+++ js/booths.js
@@ -151,12 +151,12 @@ const CONFIG = {
         { code: 'VIP Private Lounge 3', label: '3', price: '$610' },
         { code: 'VIP Private Lounge 4', label: '4', price: '$610' },
         { code: 'VIP Private Lounge 5', label: '5', price: '$610' },
-        { code: 'VIP Private Lounge 6', label: '6', price: '$610' }
+        { code: 'VIP Private Lounge 6', label: '6', price: '$610', sold: true }
       ]},
       { name: 'VIP Booths', capacity: '7 people', booths: [
         { code: 'VIP 1', label: '1', price: '$488' },
         { code: 'VIP 2', label: '2', price: '$488' },
-        { code: 'VIP 3', label: '3', price: '$488' },
+        { code: 'VIP 3', label: '3', price: '$488', sold: true },
         { code: 'VIP 4', label: '4', price: '$488', sold: true },
         { code: 'VIP 5', label: '5', price: '$488' },
         { code: 'VIP 6', label: '6', price: '$488' }
